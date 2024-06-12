a4e47db914b99bb1b3d3192e881c4f384fb8f7a6
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-12 17:21:27 +0100

    inc/minishell.h : Added some more structs, wip

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 16:11:31 by passunca          #+#    #+#             */
+/*   Updated: 2024/06/12 17:19:28 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -62,13 +74,15 @@ typedef enum e_cmd_type
 //=============================================================================/
 
 /// @struct		Token
-/// @brief		Structure for linked list to store tokens
+/// @brief		Structure to build linked list of tokens
 ///	@var type	Token type
 ///	@var val	Token string
 ///	@var len	Token length
 ///	@var to_del	Flag to delete token
 ///	@var next	Next token
-typedef struct s_token
+/// @desc		Redirection tokens: '<', '<<', '>', '>>'
+///				...
+typedef struct	s_token
 {
 	t_token_type	type;
 	char			*val;
@@ -76,6 +90,51 @@ typedef struct s_token
 	int				to_del;
 	struct s_token	*next;
 }	t_token;
+
+/// @struct			Redirection
+/// @brief	   		Structure to save redirection data
+/// @var name  		Redirection name
+/// @var flag  		Redirection status flag
+/// @var heredoc	Pointer to Heredoc redirection node
+typedef struct	s_redir
+{
+	char	*name;
+	int		flag;
+	t_list	*heredoc;
+}	t_redir;
+
+/// @struct		Command
+/// @brief		Structure to save command data
+/// @var cmd	Command string
+/// @var argc	Argument count
+/// @var argv	Argument vector
+/// @var in		Input redirection data
+/// @var out	Output redirection data
+typedef struct	s_cmd	
+{
+	char	*cmd;
+	int		argc;
+	char	**argv;
+	t_redir	*in;
+	t_redir	*out;
+}	t_cmd;
+
+/// @struct				Shell
+/// @brief				Structure to save minishell data
+/// @var cmds  			Pointer to commands list
+/// @var n_cmds			Number of commands
+/// @var exit_status	Exit status
+/// @var envp			Pointer to Environment Variables
+/// @var envt			Pointer to Temporary Environment Variables
+/// ...
+typedef struct	s_shell
+{
+	t_cmd		*cmds;
+	int			n_cmds;
+	int			exit_status;
+	char		**envp;
+	char		**envt;
+}	t_shell;
 
 //=============================================================================/
 //							Function Prototypes                                /
