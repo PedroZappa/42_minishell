9d339d642e191b87f723a997ac9bf729b3800e6b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-12 17:28:44 +0100

    src/main.c : argc / argv / envp

--- src/main.c
+++ src/main.c
@@ -6,14 +6,17 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 20:40:17 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/29 20:40:23 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/12 17:28:11 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
+	(void)argc;
+	(void)argv;
+	(void)envp;
 	ft_printf("MINISHELL\n");
 	ft_printf("Success : %d\n", SUCCESS);
 	return (0);
