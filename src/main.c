7395749384441f1c290e66355173145caf5ac66e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-13 16:04:39 +0100

    src/main.c : doxygened

--- src/main.c
+++ src/main.c
@@ -6,12 +6,25 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 20:40:17 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/12 17:28:11 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/13 16:04:14 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/**
+* @file			main.c
+* @brief		Minishell main function
+* @version		1.0
+* @date			2024/05/29
+* @author		passunca & gfragoso
+* @copyright	© 2024 passunca/gfragoso
+***/
 
 #include "../inc/minishell.h"
 
+/// @brief		Minishell main function.
+/// @param argc	Number of arguments.
+/// @param argv	Array of arguments.
+/// @param envp	Array of environment variables.
+/// @return		0 on success, 1 on failure.
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
