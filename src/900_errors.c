192705210fd9c9a32852c8a1684a6f96c00379b7
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-14 17:45:44 +0100

    src/900_errors.c : ft_err implemented

---
+++ src/900_errors.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   900_errors.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 17:35:23 by passunca          #+#    #+#             */
+/*   Updated: 2024/06/14 17:45:32 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/**
+* @file			900_errirs.c
+* @brief		Error functions
+* @version		1.0
+* @author		passunca
+***/
+
+#include "../inc/minishell.h"
+
+int	ft_err(char *msg)
+{
+	ft_putendl_fd(msg, STDERR_FILENO);
+	return (FAILURE);
+}
