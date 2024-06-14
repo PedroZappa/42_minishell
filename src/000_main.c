927cc852e9c6db8a1674b603eecad6a9dd074b16
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-14 20:13:22 +0100

    src/000_main.c : ft_init() & ft_init_env() implemented, WIP

--- src/000_main.c
+++ src/000_main.c
@@ -19,9 +19,8 @@
 #include "../inc/minishell.h"
 
 static int	ft_init(t_shell *sh);
+static char	**ft_init_env(char **env);
 
-/// @brief		Global variable to store exit code.
-/// @details	Indicates a received signal.
 int	g_exit;
 
 /// @brief		Minishell main function.
@@ -41,13 +40,44 @@ int	main(int argc, char **argv, char **envp)
 	(void)envp;
 	ft_printf("MINISHELL\n");
 	ft_printf("Success : %d\n", SUCCESS);
-	ft_init(sh);
+	if (ft_init(sh) != SUCCESS)
+		ft_err(INIT_ERR, errno);
 	return (0);
 }
 
 /// @brief	
 static int	ft_init(t_shell *sh)
 {
+	sh->cmds = NULL;
+	sh->envp = NULL;
+	sh->envp = ft_init_env(sh->envp);
+	if (sh->envp != SUCCESS)
+		return (ft_err(ENV_INIT_ERR, errno));
+	sh->envt = NULL;
+	sh->path = ft_strdup("");
+	sh->home = ft_strdup("");
+	sh->heredoc = ft_strdup("");
 	ft_get_termios(STDIN_FILENO, &sh->termios);
 	return (SUCCESS);
+}
+
+static char	**ft_init_env(char **env)
+{
+	char	**new_env;
+	int		n;
+
+	while (env[n])
+		++n;
+	new_env = ft_calloc((n + 1), sizeof(char *));
+	if (new_env != SUCCESS)
+		return (ft_err(MALLOC_ERR, errno), NULL);
+	n = -1;
+	while(env[++n])
+	{
+		new_env[n] = ft_strdup(env[n]);
+		if (new_env != SUCCESS)
+			return (NULL);
+	}
+	new_env[n] = NULL;
+	return (new_env);
 }
