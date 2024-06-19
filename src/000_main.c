f681b281fba3e039c97f20a5b92678609cc1ecae
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-19 12:43:09 +0100

    src/000_main.c : sh loop

--- src/000_main.c
+++ src/000_main.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 20:40:17 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/19 11:53:58 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/19 12:41:22 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /**
@@ -18,7 +18,7 @@
 
 #include "../inc/minishell.h"
 
-// static int	ft_sh_loop(t_shell *sh);
+static int	ft_sh_loop(t_shell *sh);
 
 int	g_exit;
 
@@ -41,34 +41,23 @@ int	main(int argc, char **argv, char **envp)
 		return (ft_err(MALLOC_ERR, errno), FAILURE);
 	if (ft_init(sh, envp) != SUCCESS)
 		ft_err(INIT_ERR, errno);
-	// ft_sh_loop(sh);
+	ft_sh_loop(sh);
 	ft_free_sh(sh, SUCCESS);
 	return (EXIT_SUCCESS);
 }
 
-int	ft_init(t_shell *sh, char **envp)
+static int	ft_sh_loop(t_shell *sh)
 {
-	sh->envp = ft_init_env(envp);
-	sh->envt = ft_calloc(1, sizeof(char *));
-	if (!sh->envp || !sh->envt)
-		return (ft_err(ENV_INIT_ERR, errno), FAILURE);
-	sh->home = ft_get_var("HOME", sh->envp, NULL);
-	sh->heredoc = ft_strdup("");
-	ft_get_termios(STDIN_FILENO, &sh->termios);
-	return (SUCCESS);
-}
+	char	*line_buf;
+	int		status;
 
-// static int	ft_sh_loop(t_shell *sh)
-// {
-// 	char	*line_buf;
-// 	int		status;
-//
-// 	(void)sh;
-// 	status = 0;
-// 	line_buf = NULL;
-// 	while (1)
-// 	{
-// 		// ft_sigset();
-// 		// status = ft_parser(sh, &line_buf);
-// 	}
-// }
+	(void)sh;
+	status = 0;
+	line_buf = NULL;
+	// while (1)
+	// {
+	// 	// ft_sigset();
+	// 	// status = ft_parser(sh, &line_buf);
+	// }
+	return (status);
+}
