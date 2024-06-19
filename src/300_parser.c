3e333517eb6f5de4123a8ba4f37a85727d785039
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-19 16:03:53 +0100

    src/300_parser.c : implementation plan added to comments

--- src/300_parser.c
+++ src/300_parser.c
@@ -6,12 +6,27 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:48:25 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/19 15:48:28 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/19 16:03:26 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+/// @brief			Parser
+/// @param sh		Pointer to a t_shell struct
+/// @param line_buf	Line buffer
+/// @return			Return 0 on success, 1 on failure
+/// @details		- Call readline to display prompt
+///					- Call tokenizer
+///					- Handle case where tk expands to a blank
+///					- Validate syntax
+///					- Count commands
+///					- Alloc cmds array
+///					- Count cmd's arguments
+///					- Initialize cmd's list
+///					- Parse cmds
+///					- Free tks
+///	@note			Used in ft_sh_loop()
 int	ft_parser(t_shell *sh, char **line_buf)
 {
 	t_token	*tks;
@@ -25,5 +40,5 @@ int	ft_parser(t_shell *sh, char **line_buf)
 	add_history(*line_buf);
 	if (ft_tokenizer(sh, line_buf, &tks) == FAILURE)
 		return (ft_free_tks(&tks));
-	return (0);
+	return (SUCCESS);
 }
