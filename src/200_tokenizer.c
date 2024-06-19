a961eac8025acd65cfb27f0fc7ea5c5bd1804b84
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-19 15:49:21 +0100

    src/200_tokenizer.c : Prototyped ft_tokenizer()

---
+++ src/200_tokenizer.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   200_tokenizer.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 15:44:44 by passunca          #+#    #+#             */
+/*   Updated: 2024/06/19 15:48:06 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+int			ft_tokenizer(t_shell *sh, char **line, t_token **tks)
+{
+	(void)sh;
+	(void)line;
+	(void)tks;
+	return (0);
+}
