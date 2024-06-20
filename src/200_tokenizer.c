826fccdd4bab76268a177fcd581e092855b92d9a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-20 17:46:00 +0100

    src/200_tokenizer.c : static functions updated

--- src/200_tokenizer.c
+++ src/200_tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:44:44 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/20 16:50:13 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/20 17:45:08 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 static int		ft_get_tkns(char *line, t_token **tks);
 static t_tk_ops	ft_get_tk(char *tk);
-static int		ft_is_tkquote(char **line);
+static int		ft_has_match(char **line);
 static char		*ft_tk_expander(t_shell *sh, char *val);
 
 /// @brief			Tokenizer
 /// @param sh		Pointer to a t_shell struct
 /// @param line		Line buffer
 /// @param tks		Pointer to a t_token struct
-/// @var tk_p		Stores the first token in the list
+/// @var tk		Stores the first token in the list
 ///					Used to traverse the tokens list
 /// @details		- Get tokens from line
 ///					- Handle Token Expansion
@@ -29,58 +29,74 @@ static char		*ft_tk_expander(t_shell *sh, char *val);
 ///						- Expand all other tokens
 int	ft_tokenizer(t_shell *sh, char **line, t_token **tks)
 {
-	t_token	*tk_p;
+	t_token	*tk;
 	char	*val;
 
 	if (ft_get_tkns(*line, tks) != SUCCESS)
 		return (ft_err(TKNZR_ERR, errno));
-	tk_p = *tks;
-	while (tk_p)
+	tk = *tks;
+	while (tk)
 	{
-		val = tk_p->val;
+		val = tk->val;
 		if (val[0] == '~' && (ft_strlen(val) == 1))
-			tk_p->val = ft_strdup(sh->home);
+			tk->val = ft_strdup(sh->home);
 		else
-			tk_p->val = ft_tk_expander(sh, val);
-		tk_p = tk_p->next;
+			tk->val = ft_tk_expander(sh, val);
+		tk = tk->next;
 		free(val);
 	}
 	return (SUCCESS);
 }
 
 /// @brief			Get tokens from line
-/// @param line		Line buffer
 /// @param tks		Pointer to a t_token struct
+/// @param line		Line buffer
+/// @var tk			Stores extracted token from line
+/// @var tmp		Pointer to the previous token
 /// @return			Return 0 on success, errno on failure
-/// @details		- Loop through line
-///						- Call ft_get_tk() to get token operation
-/// 			   		- Add token to list
+/// @details		- Stash line in tmp
+///					- Loop through line
+///						- Get token data
+///						- Store command token in the tokens list
 ///				   		- Check if line contains a matching closing quote
+/// 			   		- If a operation token was parsed:
+///							- Move pointer to next token
+///							- If the token is not a blank, add it to the list
+///					- Get last remaining remaining token
 ///	@note			Used in ft_tokenizer()
 static int	ft_get_tkns(char *line, t_token **tks)
 {
-	t_tk_ops	tk_p;
 	char		*tmp;
+	t_tk_ops	tk;
 
 	tmp = line;
 	while (line && *line)
 	{
-		tk_p = ft_get_tk(line);
-		if (tk_p.tkn != 0)
+		tk = ft_get_tk(line);
+		if ((tk.tkn != NO_TOKEN) && (tmp != line))
+			ft_tk_add(tks, ft_tk_new(tmp, (line - tmp), TK_CMD));
+		if (((*line == '\'') || (*line == '\"')) && ft_has_match(&line))
+			return (FAILURE);
+		else if (tk.tkn != NO_TOKEN)
 		{
-			line += tk_p.len;
-			ft_tk_add(tks, ft_tk_new(tk_p.tkn, tk_p.type, tk_p.len));
+			line += tk.len;
+			if (tk.type != TK_BLANK)
+				ft_tk_add(tks, ft_tk_new(tk.tkn, tk.type, tk.len));
+			tmp = line;
 		}
-		else if (ft_is_tkquote(&line) && ((*line == '\'') || (*line == '\"')))
-			return (ft_err(TKNZR_ERR, errno));
 		else
 			++line;
 	}
+	if (tmp != line)
+		ft_tk_add(tks, ft_tk_new(tmp, (line - tmp), TK_CMD));
 	return (SUCCESS);
 }
 
 /// @brief			Find matching token operation
 /// @param tk		Token string
+/// @var ops		Pointer to an array of t_tk_ops structs
+/// @var ret		Pointer to a t_tk_ops struct to be returned
+/// @var i			To iterate the array of supported tokens
 /// @return			t_tk_ops struct
 /// @details		- Iniitializes t_tk_ops array with all supported tokens
 ///					- Compares tk with each token in the array
@@ -89,7 +105,7 @@ static int	ft_get_tkns(char *line, t_token **tks)
 static t_tk_ops	ft_get_tk(char *tk)
 {
 	t_tk_ops	ops[16];
-	t_tk_ops	curr_op;
+	t_tk_ops	ret;
 	int			i;
 
 	ops[0] = (t_tk_ops){"<<", TK_IN, 2};
@@ -98,39 +114,40 @@ static t_tk_ops	ft_get_tk(char *tk)
 	ops[3] = (t_tk_ops){">|", TK_OUT, 2};
 	ops[4] = (t_tk_ops){"<>", TK_OUT, 2};
 	ops[5] = (t_tk_ops){">", TK_OUT, 1};
-	ops[6] = (t_tk_ops){" ", TK_BLANK, 1};
-	ops[7] = (t_tk_ops){"\n", TK_BLANK, 1};
-	ops[8] = (t_tk_ops){"\v", TK_BLANK, 1};
-	ops[9] = (t_tk_ops){"\t", TK_BLANK, 1};
-	ops[10] = (t_tk_ops){"\r", TK_BLANK, 1};
-	ops[11] = (t_tk_ops){"\f", TK_BLANK, 1};
-	ops[12] = (t_tk_ops){"||", TK_OR, 2};
-	ops[13] = (t_tk_ops){"&&", TK_AND, 2};
-	ops[14] = (t_tk_ops){"|", TK_PIPE, 1};
-	ops[15] = (t_tk_ops){NULL, 0, 1};
-	curr_op = (t_tk_ops){0, 0, 0};
+	ops[6] = (t_tk_ops){"||", TK_OR, 2};
+	ops[7] = (t_tk_ops){"&&", TK_AND, 2};
+	ops[8] = (t_tk_ops){"|", TK_PIPE, 1};
+	ops[9] = (t_tk_ops){NULL, 0, 1};
+	ops[10] = (t_tk_ops){" ", TK_BLANK, 1};
+	ops[11] = (t_tk_ops){"\n", TK_BLANK, 1};
+	ops[12] = (t_tk_ops){"\v", TK_BLANK, 1};
+	ops[13] = (t_tk_ops){"\t", TK_BLANK, 1};
+	ops[14] = (t_tk_ops){"\r", TK_BLANK, 1};
+	ops[15] = (t_tk_ops){"\f", TK_BLANK, 1};
+	ret = (t_tk_ops){0, 0, 0};
 	i = -1;
 	while (ops[++i].tkn)
 		if (!ft_strncmp(tk, ops[i].tkn, ops[i].len))
 			return (ops[i]);
-	return (curr_op);
+	return (ret);
 }
 
 /// @brief			Check if line contains a matching closing quote
-/// @param line		Line buffer
+/// @param quote	Pointer to a quote
 /// @var line_0		Pointer to store the start of the line
 /// @return			0 on success, 1 on failure
-/// @details		Used to check a line for a matching closing quote
+/// @details		- Checks if line contains a matching closing quote
+/// 					- If it doesn't, move pointer to end of the line
 /// @note			Used in ft_get_tkns() when a quote is parsed
-static int	ft_is_tkquote(char **line)
+static int	ft_has_match(char **quote)
 {
 	char	*line_0;
 
-	line_0 = (*line);
-	(*line) = ft_strchr(((*line) + 1), **line);
-	if (!(*line))
+	line_0 = (*quote);
+	(*quote) = ft_strchr(((*quote) + 1), **quote);
+	if (!(*quote))
 	{
-		(*line) = line_0 + ft_strlen(line_0);
+		(*quote) = line_0 + ft_strlen(line_0);
 		return (FAILURE);
 	}
 	return (SUCCESS);
