bee05a3ce55cb8745ee552bffd83638182bf10b0
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-20 18:58:31 +0100

    src/400_signal.c : logic to handle SIGINT and SIGQUIT

--- src/400_signal.c
+++ src/400_signal.c
@@ -6,10 +6,28 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:46:27 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/19 12:47:08 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/20 18:54:25 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/minishell.h"
+
+static void	ft_signal_handler(int signo);
+
 void		ft_sigset(void)
 {
+	signal(SIGINT, ft_signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+static void	ft_signal_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		rl_replace_line("", 0);
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_redisplay();
+		g_exit = EXIT_SIGINT;
+	}
 }
