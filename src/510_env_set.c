c0714f990ae5dcc6a56fd1da50a8bc4bddb8b0d4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-21 10:55:53 +0100

    src/510_env_set.c : ft_env_add_var() implemented & commented

--- src/510_env_set.c
+++ src/510_env_set.c
@@ -6,11 +6,13 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 09:15:42 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/21 10:18:47 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/21 10:54:15 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+static char	**ft_env_add_var(char **env, char *new_var);
 
 /// @brief			Set environment variable
 /// @param var		Pointer to variable to be set
@@ -59,7 +61,8 @@ int	ft_set_var(char *var, char *val, char ***env)
 /// @details		- Check if env is NULL
 /// 				- Get variable to search for length
 ///					- Loop through environment variables
-///						- If variable is found, return its index
+///						- If variable is found and it ends with = or \0,
+///						return its index,
 ///	@note			Used in ft_set_var() ...
 int	ft_var_from_env(char *var, char **env)
 {
@@ -73,15 +76,46 @@ int	ft_var_from_env(char *var, char **env)
 	while (env[++i])
 	{
 		if ((ft_strncmp(var, env[i], len) == SUCCESS) \
-			&& (env[i][len] == '=') || (env[i][len] == '\0'))
+			&& ((env[i][len] == '=') || (env[i][len] == '\0')))
 			return (i);
 	}
 	return (NO_VAR);
 }
 
-char	**ft_env_add_var(char **env, char *new_var)
+/// @brief			Add new variable to env
+/// @param env		Pointer to array of environment variables
+/// @param new_var	Pointer to new variable to add to env
+/// @var new_env	Pointer to new array of environment variables
+/// @var i			Environment variables iterator
+/// @return			SUCCESS(Pointer to new env array) or FAILURE(NULL)
+/// @details		- Loop through old environment variables
+///					- Count variables in old env
+///					- Increment var count by 2 to account for new var & NULL
+///					- Allocate new array
+///					- Copy old env to new array
+///					- Add new var
+///					- NULL terminal new array
+///					- Free old env
+///	@note			Used in ft_set_var() ...
+static char	**ft_env_add_var(char **env, char *new_var)
 {
-	(void)env;
-	(void)new_var;
-	return (NULL);
+	char	**new_env;
+	int		i;
+
+	if (!env)
+		return (ft_err(ENV_NULL_ERR, errno), NULL);
+	i = 0;
+	while (env[i])
+		++i;
+	i += 2;
+	new_env = ft_calloc(i, sizeof(char *));
+	if (!new_env)
+		return (ft_err(MALLOC_ERR, errno), NULL);
+	i = -1;
+	while (env[++i])
+		new_env[i] = ft_strdup(env[i]);
+	new_env[i] = new_var;
+	new_env[++i] = NULL;
+	ft_free_arr(env);
+	return (new_env);
 }
