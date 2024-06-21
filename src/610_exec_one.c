0da93189a2e0c082e0dbe716d811b356806e48e9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-21 21:33:26 +0100

    src/610_exec_one.c : ft_exec_one() implemented

--- src/610_exec_one.c
+++ src/610_exec_one.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 20:51:40 by passunca          #+#    #+#             */
-/*   Updated: 2024/06/21 20:58:12 by passunca         ###   ########.fr       */
+/*   Updated: 2024/06/21 21:29:53 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /**
@@ -18,17 +18,33 @@
 
 #include "../inc/minishell.h"
 
-static int	ft_exec_fork(t_shell *sh);
+static int	ft_exec_check(char *cmd);
 
-int	ft_exec_one(t_shell *sh, t_cmd *cmd)
+/// @brief			Execute one command
+/// @param sh		Pointer to a t_shell struct
+/// @var cmd		Command type
+/// @return			SUCCESS(0) or FAILURE(1)
+/// @details		- Check command type
+///					
+int	ft_exec_one(t_shell *sh)
 {
-	(void)sh;
-	(void)cmd;
-	return (SUCCESS);
+	int	cmd;
+
+	cmd = CMD_EXEC;
+	if (sh->cmds[0].argv[0] != NULL)
+		cmd = ft_exec_check(sh->cmds[0].argv[0]);
+	if (cmd == CMD_EXIT)
+		if (ft_exec(sh, cmd, 0))
+			return (ft_free_arr(sh->path), FAILURE);
+	return (ft_free_arr(sh->path), SUCCESS);
 }
 
-static int	ft_exec_fork(t_shell *sh)
+static int	ft_exec_check(char *cmd)
 {
-	(void)sh;
+	int	type;
+
+	type = CMD_EXEC;
+	if (ft_strncmp(cmd, "exit", 4) == SUCCESS)
+		type = CMD_EXIT;
 	return (SUCCESS);
 }
