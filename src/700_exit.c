e8087c5d822c7b39181aa4f03df03910ea737cdd
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-06-25 17:01:09 +0100

    src/600_execute.c : Commented

--- src/700_exit.c
+++ src/700_exit.c
@@ -19,13 +19,26 @@
 
 #include "../inc/minishell.h"
 
+static void	ft_kill(t_shell *sh, int sig);
+
 /// @brief			exit built-in
 /// @param sh		Pointer to a t_shell struct
 /// ...
-int	ft_exit(t_shell *sh)
+int	ft_exit(t_shell *sh, int n)
 {
-	(void)sh;
+	if (sh->cmds[n].argc == 1)
+		ft_kill(sh, 0);
+	else if (sh->cmds[n].argc > 1)
+		g_exit = ft_err("exit: too many arguments", FAILURE);
 	return (SUCCESS);
 }
+
+static void	ft_kill(t_shell *sh, int sig)
+{
+	ft_putstr_fd("exit\n ", STDOUT_FILENO);
+	errno = sig;
+	ft_free_sh(sh);
+	exit(g_exit);
+}	
 
 /** @} */
