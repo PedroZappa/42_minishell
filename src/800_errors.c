ca97587a437302064c5773a3574b343026e960d0
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-03 10:47:30 +0100

    src/800_errors.c : commented

--- src/800_errors.c
+++ src/800_errors.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 17:35:23 by passunca          #+#    #+#             */
-/*   Updated: 2024/07/03 10:24:14 by passunca         ###   ########.fr       */
+/*   Updated: 2024/07/03 10:47:12 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /**
@@ -35,7 +35,8 @@ int	ft_err(char *msg, int status)
 
 /// @brief Create error message
 /// @details
-/// -
+/// - Build error message
+/// - Print error message
 /// @param tkn		Message to be printed
 /// @param err		Error number
 /// @return			SUCCESS(0)
@@ -44,29 +45,31 @@ int	ft_syntax_err(char *tkn, int err)
 	char	*start;
 	char	*end;
 
-	start = ft_strjoin("Syntax error near unexpected token'", tkn);
-	end = ft_strjoin(start, "'");
+	start = ft_strjoin_free("Syntax error near unexpected token'", tkn);
+	end = ft_strjoin_free(start, "'");
 	ft_err(end, err);
-	free(start);
-	free(end);
 	return (SUCCESS);
 }
 
+/// @brief			Print invalid flag error
+/// @details
+/// - Build error message
+/// - Print error message
+/// @param cmd		Command name
+/// @param cmd_flag	Flag name
+/// @param err		Error number
+/// @return			SUCCESS(ret)
 int	ft_flag_err(char *cmd, char *cmd_flag, int err)
 {
 	char	*tmp;
 	char	*msg;
 	int		ret;
 
-	msg = ft_strjoin(cmd_flag, "': flag/option not supported");
-	tmp = ft_strjoin(": '", msg);
-	free(msg);
-	msg = ft_strjoin(cmd, tmp);
+	msg = ft_strjoin_free(cmd_flag, "': flag/option not supported");
+	tmp = ft_strjoin_free(": '", msg);
+	msg = ft_strjoin_free(cmd, tmp);
 	ret = ft_err(msg, err);
-	free(msg);
-	free(tmp);
 	return (ret);
-
 }
 
 /** @} */
