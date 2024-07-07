6c1e8d7f7ff19f09b0aef4efcce8c61f3255159a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-07 11:43:30 +0100

    src/740_clear.c : updated

--- src/740_clear.c
+++ src/740_clear.c
@@ -6,12 +6,26 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 11:25:33 by passunca          #+#    #+#             */
-/*   Updated: 2024/07/07 11:34:49 by passunca         ###   ########.fr       */
+/*   Updated: 2024/07/07 11:42:39 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/**
+* @defgroup 	clear	clear built-in command
+* @{
+*
+* @brief		clear screen built-in
+* @version		1.0
+***/
 
 #include "../inc/minishell.h"
 
+/// @brief			clear screen built-in
+/// @details
+/// Clear screen ANSI sequence: "\033[H\033[J"
+///		- `\033[H` : moves the cursor to the home position
+///		- `\033[J` : clears the screen from the cursor to the end of the screen
+/// @param n		Command index
+/// @return			SUCCESS(0)
 int	ft_clear(int n)
 {
 	(void)n;
@@ -19,3 +33,4 @@ int	ft_clear(int n)
 	return (SUCCESS);
 }
 
+/** @} */
