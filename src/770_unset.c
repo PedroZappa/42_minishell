c678ccdb7a556c563f92eacb2932f9588cc3210d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-09 16:51:07 +0100

    src/770.c : unset partially implemented

--- src/770_unset.c
+++ src/770_unset.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 16:37:35 by passunca          #+#    #+#             */
-/*   Updated: 2024/07/09 16:38:02 by passunca         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:50:51 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 int	ft_unset(t_shell *sh, int n)
 {
-	(void)sh;
-	(void)n;
+	char	*var;
+	int	i;
+
+	ft_set_var(sh->cmds[n].argv[sh->cmds[n].argc - 1], "_", &sh->envp);
+	if (!sh->cmds[n].argv[1] || !sh->envp)
+		return (FAILURE);
+	if (sh->cmds[n].argv[1])
+		return (ft_flag_err(sh->cmds[n].argv[0], sh->cmds[n].argv[1], 1));
+	i = 0;
+	while (sh->cmds[n].argv[++i])
+	{
+		var = sh->cmds[n].argv[i];
+		if (!ft_strchr(var, '='))
+		{
+			// TODO: ft_get_var_index() from envp, implement ft_env_del_var()
+			// TODO: ft_get_var_index() from envt
+		}
+	}
 	return (SUCCESS);
 }
