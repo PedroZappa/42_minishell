c983026d4c688b60c7e7806aef5d44bfc3a35f70
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-12 15:55:15 +0100

    src/700_exit.c : Arg handled

--- src/700_exit.c
+++ src/700_exit.c
@@ -29,10 +29,7 @@ int	ft_exit(t_shell *sh, int n)
 	if (sh->cmds[n].argc == 1)
 		ft_kill(sh, EXIT_SUCCESS);
 	else if (sh->cmds[n].argc == 2)
-	{
-		if (ft_atoi(sh->cmds[n].argv[1]) < 0)
-			ft_kill(sh, ft_abs(*sh->cmds[n].argv[1]));
-	}	
+		ft_kill(sh, ft_abs(ft_atoi(sh->cmds[n].argv[1])));
 	else if (sh->cmds[n].argc > 2)
 		ft_fprintf(STDERR_FILENO, "exit\nbash: %s: too many arguments\n",
 			sh->cmds[n].argv[0], sh->cmds[n].argv[1]);
