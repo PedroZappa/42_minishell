a303d044e60c72104d513497633bd794302b49d8
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-16 11:26:22 +0100

    src/670.c : ft_pipe_setter() implemented

--- src/670_pipes.c
+++ src/670_pipes.c
@@ -6,11 +6,22 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 11:19:37 by passunca          #+#    #+#             */
-/*   Updated: 2024/08/16 11:25:16 by passunca         ###   ########.fr       */
+/*   Updated: 2024/08/16 11:26:02 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+/// @brief			Set pipes
+/// @param pipe		Pointer to a pipe
+/// @param end		End of pipe (read or write)
+/// @return			SUCCESS(0)
+int	ft_pipe_setter(int *pipe, int end)
+{
+	if (pipe[end] != end)
+		return (dup2(pipe[end], end));
+	return (SUCCESS);
+}
 
 /// @brief			Close pipes
 /// @param pipe0	Pointer to a pipe0
@@ -27,15 +38,4 @@ void	ft_close_pipes(int *pipe0, int *pipe1)
 		close(pipe1[0]);
 		close(pipe1[1]);
 	}
-}
-
-/// @brief			Set pipes
-/// @param pipe		Pointer to a pipe
-/// @param end		End of pipe (read or write)
-/// @return			SUCCESS(0)
-int	ft_pipe_setter(int *pipe, int end)
-{
-	if (pipe[end] != end)
-		return (dup2(pipe[end], end));
-	return (SUCCESS);
 }
