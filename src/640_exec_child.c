3823175213b86f3586d777fceaac48a818d8c7c4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-17 10:56:20 +0100

    src/640_exec_child.c : ft_exec_child_i() implemented, still needs redirs

--- src/640_exec_child.c
+++ src/640_exec_child.c
@@ -42,10 +42,30 @@ void	ft_exec_child_first(t_shell *sh, int *outpipe)
 
 int	ft_exec_child_i(t_shell *sh, int **pipes, int i)
 {
-	(void)sh;
-	(void)pipes;
-	(void)i;
-	return (SUCCESS);
+	// TODO: Handle redir in
+	// if (sh->cmds[i].in.name)
+	// 	ft_redir_in(sh, i);
+	// 	else if...
+	if (ft_pipe_setter(pipes[0], STDIN_FILENO) == PIPE_FAIL)
+	{
+		ft_close_pipes(pipes[0], pipes[1]);
+		ft_fork_exit(sh, PIPE_ERR, FAILURE);
+	}
+	// TODO: Handle redir out
+	// if (sh->cmds[i].out.name)
+	// 	ft_redir_out(sh, i);
+	// 	else if...
+	if (ft_pipe_setter(pipes[1], STDOUT_FILENO) == PIPE_FAIL)
+	{
+		ft_close_pipes(pipes[0], pipes[1]);
+		ft_fork_exit(sh, PIPE_ERR, FAILURE);
+	}
+	ft_close_pipes(pipes[0], pipes[1]);
+	if (sh->cmds[i].argv[0])
+		ft_exec_cmd(sh, ft_exec_check(sh->cmds[i].argv[0]), i);
+	free(pipes);
+	ft_free_sh(sh);
+	exit(SUCCESS);
 }
 
 int	ft_exec_child_last(t_shell *sh, int *inpipe, int i)
