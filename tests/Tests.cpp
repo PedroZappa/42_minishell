7d70591f354e7bfc598e778c8b1178c65fa4add6
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-19 09:55:46 +0100

    CMake : Updated, trying to make it work at 42

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,12 +1,44 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <array>
+#include <string>
+#include <memory>
 
-TEST(Tests, TestTest)
-{
-	EXPECT_TRUE(true);
+// BASH_PATH will be defined from CMakeLists.txt
+#ifndef BASH_PATH
+# define BASH_PATH "/bin/bash"
+#endif
+
+std::string run_command(const std::string& shell, const std::string& command) {
+    std::array<char, 128> buffer;
+    std::string result;
+    
+    // Prepare the full command to execute the shell with the provided command
+    std::string full_command = shell + " -c \"" + command + "\"";
+    
+    std::shared_ptr<FILE> pipe(popen(full_command.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    
+    // Read the command output
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    
+    return result;
 }
 
-int	main(int argc, char **argv)
-{
-	testing::InitGoogleTest(&argc, argv);
-	return (RUN_ALL_TESTS());
+TEST(MiniShellTest, CompareOutputs) {
+    // Define the command to be tested
+    std::string command = "echo Hello World";
+
+    // Run the command with minishell
+    std::string minishell_output = run_command("./minishell", command);
+
+    // Run the command with bash
+    std::string bash_output = run_command(BASH_PATH, command);
+
+    // Compare outputs
+    ASSERT_EQ(minishell_output, bash_output) << "Output mismatch between minishell and bash";
 }
