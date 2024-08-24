6e99ce1d3bd6e744939c7236a2daf5f827ec3acb
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-24 10:02:21 +0100

    tests/tester.cpp : Tester class moved to own file

--- tests/main.cpp
+++ tests/main.cpp
@@ -9,19 +9,3 @@ int main(int argc, char** argv) {
 
 	return RUN_ALL_TESTS();
 }
-
-std::string Tester::exec(const char* cmd) {
-	std::array<char, 128> buffer;
-	std::string result;
-
-	
-	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-	if (!pipe) {
-		throw std::runtime_error("popen() failed!");
-	}
-	while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
-		result += buffer.data();
-	}
-	return result;
-}
-
---
+++ tests/tester.cpp
@@ -0,0 +1,18 @@
+#include "tester.hpp"
+
+std::string Tester::exec(const char* cmd) {
+	std::array<char, 128> buffer;
+	std::string result;
+
+	
+	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+	if (!pipe) {
+		throw std::runtime_error("popen() failed!");
+	}
+	while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
+		result += buffer.data();
+	}
+	return result;
+}
+
+
