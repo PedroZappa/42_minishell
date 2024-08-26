b716bd80568974974c74ca0fab3fdb6f686d1b90
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-26 10:27:26 +0100

    expander : wip

--- src/200_tokenizer.c
+++ src/200_tokenizer.c
@@ -145,12 +145,22 @@ static t_tk_ops	ft_get_tk(char *tk, t_tk_ops **ops)
 	i = 0;
 	if (!ft_isspace(tk[0]))
 	{
-		if ((tk[0] == '\'') || (tk[0] == '\"'))
+		if (tk[0] == '\"')
 		{
 			++i;
-			while ((tk[i] && (tk[i] != '\'')) && (tk[i] != '\"'))
+			while (tk[i] && (tk[i] != '\"'))
 				++i;
-			if (tk[i] == '\'' || tk[i] == '\"')
+			if (tk[i] == '\"')
+				++i;
+			ret = (t_tk_ops){ft_substr(tk, 0, i), TK_CMD, i};
+			return (ret);
+		}
+		if (tk[0] == '\'')
+		{
+			++i;
+			while (tk[i] && (tk[i] != '\''))
+				++i;
+			if (tk[i] == '\'')
 				++i;
 			ret = (t_tk_ops){ft_substr(tk, 0, i), TK_CMD, i};
 			return (ret);
