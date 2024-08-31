34fbb6e8c5368b78f87168ef4dea8a4b6ff827c1
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-08-31 01:55:35 +0100

    Update 660_execve.c

--- src/660_execve.c
+++ src/660_execve.c
@@ -30,7 +30,7 @@ void	ft_execve(char **path, char **argv, char **envp)
 {
 	int	execve_err;
 
-	if ((*argv[0] == '.' && *argv[1] == '/') || (*argv[0] == '/'))
+	if ((argv[0][0] == '.' && argv[0][1] == '/') || (argv[0][0] == '/'))
 	{
 		execve_err = execve(*argv, argv, envp);
 		if (execve_err != EXECVE_ERR)
