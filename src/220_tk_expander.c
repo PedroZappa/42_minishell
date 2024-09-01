9998b8bd580413a416e9b6fa343f99f56ac8736c
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-09-01 03:43:19 +0100

    Update 220_tk_expander.c

--- src/220_tk_expander.c
+++ src/220_tk_expander.c
@@ -90,6 +90,8 @@ char	*ft_expand_dollar(t_shell *sh, char *tkn, int *i)
 	}
 	else if (tkn[*i] == '?')
 		*i += 1;
+	else if ((tkn[*i] == '\'' || tkn[*i] == '\"') && tkn[*i + 1] != '\0')
+		return (ft_strdup(""));
 	else
 		return (ft_strdup("$"));
 	temp = ft_substr(tkn, j, *i - j);
