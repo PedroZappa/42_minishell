e9d2fed1f21dc3122429374fe144e633d65409cc
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-25 12:11:13 +0000

    Trying to fix clangd inside neovim

--- flake.nix
+++ flake.nix
@@ -1,100 +1,110 @@
 {
-  description = "C/C++ environment";
-
+  description = "Neovim flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     self,
     nixpkgs,
-    utils,
-    ...
-  } @ inputs:
-    utils.lib.eachDefaultSystem (
+    flake-utils,
+  }:
+    {
+      overlay = final: prev: let
+        pkgs = nixpkgs.legacyPackages.${prev.system};
+      in rec {
+        neovim = pkgs.neovim-unwrapped.overrideAttrs (oa: {
+          version = "master";
+          src = ../.;
+          buildInputs =
+            oa.buildInputs
+            ++ [
+              pkgs.tree-sitter
+            ];
+          cmakeFlags =
+            oa.cmakeFlags
+            ++ [
+              "-DUSE_BUNDLED=OFF"
+            ];
+        });
+        # a development binary to help debug issues
+        neovim-debug = let
+          stdenv = pkgs.stdenvAdapters.keepDebugInfo (
+            if pkgs.stdenv.isLinux
+            then pkgs.llvmPackages_latest.stdenv
+            else pkgs.stdenv
+          );
+        in
+          pkgs.enableDebugging ((neovim.override {
+              lua = pkgs.enableDebugging pkgs.luajit;
+              inherit stdenv;
+            })
+            .overrideAttrs (oa: {
+              cmakeBuildType = "Debug";
+              cmakeFlags =
+                oa.cmakeFlags
+                ++ [
+                  "-DMIN_LOG_LEVEL=0"
+                ];
+              disallowedReferences = [];
+            }));
+        # for neovim developers, builds a slow binary
+        # huge closure size but aims at covering all scripts
+        # brings development tools as well
+        neovim-developer = let
+          lib = nixpkgs.lib;
+          pythonEnv = pkgs.python3.withPackages (ps: [
+            ps.msgpack
+            ps.flake8 # for 'make pylint'
+          ]);
+          luacheck = pkgs.luaPackages.luacheck;
+        in
+          (neovim-debug.override {doCheck = pkgs.stdenv.isLinux;}).overrideAttrs (oa: {
+            cmakeFlags =
+              oa.cmakeFlags
+              ++ [
+                "-DLUACHECK_PRG=${luacheck}/bin/luacheck"
+                "-DMIN_LOG_LEVEL=0"
+                "-DENABLE_LTO=OFF"
+                "-DUSE_BUNDLED=OFF"
+              ]
+              ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+                # https://github.com/google/sanitizers/wiki/AddressSanitizerFlags
+                # https://clang.llvm.org/docs/AddressSanitizer.html#symbolizing-the-reports
+                "-DCLANG_ASAN_UBSAN=ON"
+              ];
+            nativeBuildInputs =
+              oa.nativeBuildInputs
+              ++ (with pkgs; [
+                pythonEnv
+                include-what-you-use # for scripts/check-includes.py
+                jq # jq for scripts/vim-patch.sh -r
+                shellcheck # for `make shlint`
+                doxygen # for script/gen_vimdoc.py
+                clang-tools # for clangd to find the correct headers
+              ]);
+
+            shellHook =
+              oa.shellHook
+              + ''
+                export NVIM_PYTHON_LOG_LEVEL=DEBUG
+                export NVIM_LOG_FILE=/tmp/nvim.log
+                export ASAN_OPTIONS="log_path=./test.log:abort_on_error=1"
+                export UBSAN_OPTIONS=print_stacktrace=1
+              '';
+          });
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {inherit system;};
-        llvm = pkgs.llvmPackages_latest;
-        lib = nixpkgs.lib;
-
-        # simple script which replaces the functionality of make
-        # it works with <math.h> and includes debugging symbols by default
-        # it will be updated as per needs
-
-        # arguments: outfile
-        # basic usage example: mk main [flags]
-        mymake = pkgs.writeShellScriptBin "mk" ''
-          if [ -f "$1.c" ]; then
-            i="$1.c"
-            c=$CC
-          else
-            i="$1.cpp"
-            c=$CXX
-          fi
-          o=$1
-          shift
-          $c -ggdb $i -o $o -lm -Wall $@
-        '';
-      in {
-        # DEV SHELL
-        devShell =
-          pkgs.mkShell.override {
-            stdenv = pkgs.clangStdenv;
-          } rec {
-            name = "C";
-
-            nativeBuildInputs = [
-              pkgs.cmake
-              llvm.lldb
-              pkgs.clang-tools
-              llvm.clang
-              pkgs.gtest
-            ];
-
-            buildInputs = [
-              pkgs.libcxx
-            ];
-            CPATH = builtins.concatStringsSep ":" [
-              (lib.makeSearchPathOutput "dev" "include" [llvm.libcxx])
-              (lib.makeSearchPath "resource-root/include" [llvm.clang])
-            ];
-            # CPATH = "~/.nix-profile/include";
-            LIBRARY_PATH = "~/.nix-profile/lib";
-            QTDIR = "~/.nix-profile";
-
-            packages = with pkgs; [
-              # builder
-              gnumake
-              cmake
-              bear
-
-              # debugger
-              llvm.lldb
-              gdb
-
-              # fix headers not found
-              clang-tools
-
-              # LSP and compiler
-              llvm.libstdcxxClang
-
-              # other tools
-              cppcheck
-              llvm.libllvm
-              valgrind
-              mymake
-
-              # stdlib for cpp
-              llvm.libcxx
-
-              # libs
-              glm
-              SDL2
-              SDL2_gfx
-            ];
-          };
+        pkgs = import nixpkgs {
+          overlays = [self.overlay];
+          inherit system;
+        };
+      in rec {
+        packages = with pkgs; {
+          inherit neovim neovim-debug neovim-developer;
+        };
       }
     );
 }
-
